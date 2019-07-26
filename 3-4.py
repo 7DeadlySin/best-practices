2a76cd85a57c6f04a56feb380dd0e18654ffec17
Boss <bossdee@hotmail.com>
2019-07-26 16:21:54 +0700

    Add files via upload

---
+++ 3-4.py
@@ -0,0 +1,12 @@
+
+
+n = 5
+
+for i in range(0, n):
+    for j in range(0,n):
+        if j == i or j == n-1-i:
+            print("*", end = "")
+        else:
+            print(" ", end = "")
+    print("")
+    
